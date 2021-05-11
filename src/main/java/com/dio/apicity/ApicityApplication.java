b58dc02c96cec14d9cfa2be6bdab1f20494b15cc
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-05-11 02:45:50 -0300

    connections with GitKraken

---
+++ src/main/java/com/dio/apicity/ApicityApplication.java
@@ -0,0 +1,13 @@
+package com.dio.apicity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicityApplication.class, args);
+	}
+
+}
